180d8eb73573564f79dfb06d0eb46cd79157c658
Ubuntu <ubuntu@ip-172-31-9-218.ap-northeast-1.compute.internal>
2016-09-14 06:04:00 +0000

    1. Add the first commit.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # iiot-hmac-request
+
+Generate Hmac Authorization Header
+
+# Example #
+
+```
+var IiotHmac = require('iiot-hmac-request');
+
+var hmac = new IiotHmac({
+  iiotAccessKeyId : "YOUR_KEY_ID",
+  iiotSecretKey   : "YOUR_KEY_SECRET"
+});
+
+var headers = hmac.getHeaders();
+// -->
+// {
+//    "Date":"Wed, 14 Sep 2016 06:01:46 GMT",
+//    "Authorization":"IIOT e9264147b413746293e539868c8edff71715e1e3:zFkviwleO1MjN9U4y4RN5H9r9qP7d11axe7gDIgaVrM="
+// }
+```
---
+++ index.js
@@ -0,0 +1,27 @@
+var crypto = require("crypto");
+var d = new Date().toUTCString();
+
+var generateHmac = function(data, iiotSecretKey, algorithm, encoding) {
+  encoding = encoding || "base64";
+  algorithm = algorithm || "sha256";
+
+  console.log(crypto.createHmac(algorithm, iiotSecretKey).update(data).digest(encoding));
+  return crypto.createHmac(algorithm, iiotSecretKey).update(data).digest(encoding);
+}
+
+module.exports = function(config) {
+
+  var options = {
+    "headers" : {
+      "Date" : d,
+      "Authorization" : "IIOT " + config.iiotAccessKeyId +
+        ":" + generateHmac(d, config.iiotSecretKey)
+    }
+  };
+
+  this.getHeaders = function() {
+    return options.headers;
+  }
+
+  return this;
+}
